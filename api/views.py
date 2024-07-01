3a663c695e8d744b09a77ae2d82d79bb7739357f
Mutukukioko <kharrylungu@gmail.com>
2024-07-01 01:24:17 -0700

    third deploy

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    content="""
+    <div style='width:100%; height:100vh; display:flex; justify-content:center; align-items:center'>
+    <h1  style='color:green;'>django</h1>
+    </div>
+    """
+    return HttpResponse(content)
